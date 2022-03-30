860e7852bfd6da4f1d8582110795caf8bb926552
Jain Joseph k <jainbrm@gmail.com>
2022-03-30 17:51:03 +0400

    Function 2 & 3 Artuments

---
+++ Functions2.java
@@ -0,0 +1,14 @@
+
+public class Functions2 {
+	public static void main(String arg[]) {
+		display();
+	}
+	public static void display() {
+		System.out.println("Function without Arg & Without Rtn");
+		System.out.println("-----------------------------------");
+		System.out.println("                                    ");System.out.println("Jain Joseph");
+		System.out.println("IbrahimKhudabaksh & Sons LLC");
+		System.out.println("p.B.No.18, Postal Code:512");
+		System.out.println("Al Buraimi, Sultanate of Oman");
+	}
+}
---
+++ Functions3.java
@@ -0,0 +1,11 @@
+
+public class Functions3 {
+	// Function with argument and without return
+	public static void main(String arg[]) {
+		display(2,5);
+	}
+	public static void display(int num1,int num2){
+		System.out.println("Result = "+(num1+num2));
+		System.out.println("=================");
+	}
+}
