c6b85df0c470cd0bd154de72283d0ad065188806
Jain Joseph K <jainbrm@gmail.com>
2022-03-31 08:37:41 +0400

    Function2 office

---
+++ Functions2/src/Functions2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+// Java Functions without Argument & witho Returm
+//to display address
+public class Functions2 {
+	public static void main(String a[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Number");
+		int num1 = sc.nextInt();
+		address();
+				
+				
+	}
+	// using void for without argument & return nil
+	 static void address() {
+		System.out.println("Jain");
+		System.out.println("Ibrahim Khudabaksh & Sons LLC");
+		System.out.println("P.O Box: 18, Postal Code: 512");
+		System.out.println("AL BURAIMI");
+		System.out.println("Sultanate of Oman");
+	}
+}
